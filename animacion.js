d280e09c19b0e3850a905d9448e06fef046aaf81
Vicente Roa <79950811+vicenteroa@users.noreply.github.com>
2023-03-19 17:30:05 -0300

    Add files via upload

---
+++ animacion.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function() {
+  var imagen = document.getElementById('tu');
+  imagen.classList.add('aparecer');
+});
+window.addEventListener('load', function() {
+  var imagen = document.getElementById('te-conoci');
+  imagen.classList.add('aparecer');
+});
+window.addEventListener('load', function() {
+  var imagen = document.getElementById('t-inicio');
+  imagen.classList.add('aparecer');
+});
